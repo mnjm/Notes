34dea162cd853842909e310a48b120f91d2f2be4
Manjunath Mohan <manjunat.mohan@gmail.com>
2022-10-30 10:55:18 +0530

    vault backup: 2022-10-30 10:55:18 mmlinux\n\nFiles:

--- git-sync.sh
+++ git-sync.sh
@@ -7,11 +7,14 @@ if [ ! -d "./.obsidian" ]; then
     exit 3
 fi
 
-changeExists="$(git status --porcelain | wc -l)"
+changes="$(git status --porcelian)"
+changeExists="$(echo $changes | wc -l)"
 
 if [ $changeExists -gt 0 ]; then
     git add .
-    git commit -m "vault backup: $(date +"%Y-%m-%d %H:%M:%S") $hostname"
+    git commit -m "vault backup: $(date +"%Y-%m-%d %H:%M:%S") $(hostname)\n\nFiles:$changes"
     git pull --rebase
     git push origin main
+else
+    git pull
 fi 
